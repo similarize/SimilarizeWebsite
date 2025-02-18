bd213097e74ededb0dfdffc695fc84019d337d9f
Ben@Flight <ben@flightenabled.com>
2025-02-18 13:24:03 -0700

    update batch file for title

--- game/build.bat
+++ game/build.bat
@@ -1,6 +1,6 @@
 @echo off
 rm game.apk
-python -m pygbag --build .
+python -m pygbag --build  --template index.html.tpl --title "Loading RC Rally Jump" .
 move build\web\* .
 rmdir /s /q build
 pause
